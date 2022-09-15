27687c478cd02240b28e58463fe9fa3e6a387083
yexiunr <113634354+yexiunr@users.noreply.github.com>
2022-09-16 04:37:46 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -64,7 +64,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oQ9_d6vMNi2JETwx31223jp1Fcxc",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "53tJhNj9aJxzwFNyptoiNPtKJg_Uvgjm0jA-P-sRa9M",
+      useTemplateId: "or_zUrPS-wZqkAlszoQt3geX7SxkOAEyr9mlgY_Y-Sc",
       // 所在省份
       province: "贵州",
       // 所在城市
